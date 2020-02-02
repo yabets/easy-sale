801a1ad1dab12d88845a89dfc309bbbb36f62227
yabets <yabetsbelay@gmail.com>
2020-02-02 23:40:13 +0530

    Spring Boot initialize

---
+++ src/main/java/com/easy/shop/EasyShopApplication.java
@@ -0,0 +1,13 @@
+package com.easy.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyShopApplication.class, args);
+	}
+
+}
